d4ddbf8a55c891db40078f1000dd6444f38af7c3
Juanjo G. Melgar <sirtep@gmail.com>
2022-06-17 15:43:21 +0200

    Java files

---
+++ java/constantes.java
@@ -0,0 +1,12 @@
+package main;
+
+public class constantes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int test = 10;
+		final int test2 = test;
+	}
+
+}
